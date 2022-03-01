dd565a67cbb5ef6c968673025505b473262a14cc
Oskar Boysen <oskarboysen1@gmail.com>
2022-03-01 13:50:23 +0100

    Database setup
    Indsæt tabeller

--- src/main/java/dk/sdu/se_f22/BrandModule/Management/persistence/Persistence.java
+++ src/main/java/dk/sdu/se_f22/BrandModule/Management/persistence/Persistence.java
@@ -18,11 +18,30 @@ public class Persistence implements IPersistence {
         } catch (Exception e) {
             System.err.println(e.getClass().getName()+": "+e.getMessage());
         }
+
+        // Make sure tables are always created
+        setupDatabase();
     }
 
     @Override
     public void createBrand(Brand brand) {
+        try {
+            var stmt = c.createStatement();
 
+            var insert =  "INSERT INTO brand (name, description , founded, headquarters)" +
+            String.format("VALUES (%s, %s, %s,%s, %s);", brand.name, brand.description, brand.founded, brand.headquarters);
+
+            for (var product : brand.products) {
+                stmt.addBatch("INSERT INTO Product VALUES()");
+            }
+
+            stmt.executeUpdate(insert);
+            stmt.close();
+            c.close();
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
     }
 
     @Override
@@ -44,12 +63,31 @@ public class Persistence implements IPersistence {
     public void setupDatabase() {
         try {
             var stmt = c.createStatement();
-            String sql = "CREATE TABLE IF NOT EXISTS Brand " +
+
+            var brandTableSql =  "CREATE TABLE IF NOT EXISTS Brand " +
                     "(id serial PRIMARY KEY," +
                     "name VARCHAR(255) UNIQUE NOT NULL, " +
                     "description VARCHAR(10000), " +
                     "founded VARCHAR(255), " +
                     "headquarters VARCHAR(255));";
+
+            var productTableSql = "CREATE TABLE IF NOT EXISTS Product(" +
+                    "id serial PRIMARY KEY," +
+                    "name VARCHAR(255) UNIQUE NOT NULL);";
+
+            var brandProductTableSql = "CREATE TABLE IF NOT EXISTS BrandProduct(" +
+                    "Brandid INTEGER REFERENCES Brand(id)," +
+                    "Productid INTEGER REFERENCES Product(id));";
+
+            var configTableSql = "CREATE TABLE IF NOT EXISTS config(" +
+                    "indexinterval INTEGER NOT NULL" +
+                    ");";
+
+            var sql = brandTableSql +
+                    productTableSql +
+                    brandProductTableSql +
+                    configTableSql;
+
             stmt.executeUpdate(sql);
             stmt.close();
             c.close();
@@ -57,11 +95,5 @@ public class Persistence implements IPersistence {
         catch (Exception e) {
             System.out.println(e);
         }
-    }
-
-    // Testing connection to Postgres database
-    public static void main(String[] args) {
-        Persistence persistence = new Persistence();
-        persistence.setupDatabase();
     }
 }
--- src/main/java/dk/sdu/se_f22/BrandModule/Management/services/JsonService.java
+++ src/main/java/dk/sdu/se_f22/BrandModule/Management/services/JsonService.java
@@ -32,10 +32,10 @@ public class JsonService implements IJsonService {
 
                 Brand brand = new Brand();
                 // Set single valued properties
-                brand.setName(name.toString());
-                brand.setDescription(properties.get("description").toString());
-                brand.setFounded(properties.get("founded").toString());
-                brand.setHeadquarters(properties.get("headquarters").toString());
+                brand.name = (name.toString());
+                brand.description = (properties.get("description").toString());
+                brand.founded = (properties.get("founded").toString());
+                brand.headquarters = (properties.get("headquarters").toString());
 
                 // Build product array
                 JSONArray objs = (JSONArray) properties.get("products");
@@ -45,7 +45,7 @@ public class JsonService implements IJsonService {
                 iterator.forEachRemaining(p -> products.add(p.toString()));
 
                 // Set brand property
-                brand.setProducts(products);
+                brand.products = (products);
 
                 // Add brand to list of brands
                 brands.add(brand);
