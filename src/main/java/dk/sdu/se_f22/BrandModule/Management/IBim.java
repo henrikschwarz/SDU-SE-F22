8e191dda9c417cb9c98c2dbe5ba09784be4fcffb
v-nemeth <viand21@student.sdu.dk>
2022-03-03 10:56:08 +0100

    Added Bim-Interface to branch

--- src/main/java/dk/sdu/se_f22/BrandModule/Management/IBim.java
+++ src/main/java/dk/sdu/se_f22/BrandModule/Management/IBim.java
@@ -1,4 +1,19 @@
 package dk.sdu.se_f22.BrandModule.Management;
 
+import dk.sdu.se_f22.SharedLibrary.models.Brand;
+
 public interface IBim {
+
+    public void createBrand(Brand brand);
+
+    public Brand getBrand(int id);
+
+    public Brand getBrand(String name);
+
+    public void removeBrand(Brand brand);
+
+    public Brand updateBrand(Brand brand);
+
+    public void setIndexInterval(int indexInterval);
+
 }
